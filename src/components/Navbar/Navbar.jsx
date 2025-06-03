e0d0beb60c660240d9407af2ce46553b23a79043
Your Name <mdsultanmahmud.bd00@gmail.com>
2025-06-03 14:40:34 +0600

    feat: responsive navbar,home page and contact us page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { useLocation, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [menu, setMenu] = useState(false);
+  const onClickHandler = (url) =>{
+    setMenu(false);
+    navigate(`${url}`);
+  }
   return (
     <div className="portfolio__navbar__container">
       <div className="logo__container">
-        <p>thi is logo</p>
+        <img
+          onClick={() => navigate("/")}
+          src={logo}
+          alt="Logo of this website!"
+        />
       </div>
-      <div className="menu__container">
+      <div className="menu__icons">
+        {!menu ? (
+          <FaBars onClick={() => setMenu(true)} size={22} />
+        ) : (
+          <ImCross onClick={() => setMenu(false)} size={22} />
+        )}
+      </div>
+      <div className={`menu__container ${menu ? "active" : ""}`}>
         <ul>
           <li
             className={`${location.pathname === "/" ? "active" : ""}`}
-            onClick={() => navigate("/")}
+            onClick={() => onClickHandler("/")}
           >
             Home
           </li>
           <li
             className={`${location.pathname === "/about" ? "active" : ""}`}
-            onClick={() => navigate("/about")}
+            onClick={() => onClickHandler("/about")}
           >
             About
           </li>
           <li
             className={`${location.pathname === "/portfolio" ? "active" : ""}`}
-            onClick={() => navigate("/portfolio")}
+            onClick={() => onClickHandler("/portfolio")}
           >
             Portfolio
           </li>
           <li
             className={`${location.pathname === "/blog" ? "active" : ""}`}
-            onClick={() => navigate("/blog")}
+            onClick={() => onClickHandler("/blog")}
           >
             Blog
           </li>
           <li
             className={`${location.pathname === "/contact" ? "active" : ""}`}
-            onClick={() => navigate("/contact")}
+            onClick={() => onClickHandler("/contact")}
           >
             Contact
           </li>
